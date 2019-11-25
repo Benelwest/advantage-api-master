02e9540fc607d7d4c1438e137e7ec2f088df7e1e
ben <benell@bluewin.ch>
2019-11-25 21:55:18 +0100

    Add Orders

--- Models/Orders.cs
+++ Models/Orders.cs
@@ -3,7 +3,10 @@ namespace Advantage.API.Models
 public class Order
     {
         public int Id { get; set;}
-        
+        public Customer Customer{get; set; }
+        public decimal OrderTotal {get; set;}    
+        public DateTime Placed {get; set;}
+        public DateTime? Completed {get; set;}                 
     }
 
 }
